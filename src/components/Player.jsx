c84f57f0f9b0bfa76ab06f5ea34a94cbe53acf16
FridaSvanberg <svanberg92@gmail.com>
2024-06-28 20:19:01 +0200

    Add Player component that can edit and save the players name

---
+++ src/components/Player.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    /* If you would run this function isEditing will not go from true to false, as it might seem.
+        Because of React scheduling, both of these lines will think that isEditing was false,
+        so both lines will do the same. React will only run the handleEditClick function
+            setIsEditing(!isEditing); => true
+            setIsEditing(!isEditing); => true
+        But if you use a function in the setIsEditing twice, it will be first true and then false.
+        Because when you run it as a function, React will run them both inside the handleEditClick function.
+            setIsEditing((editing) => !editing); => true
+            setIsEditing((editing) => !editing); => false
+    */
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        className="player-name"
+        type="text"
+        required
+        onChange={handleChange}
+        value={playerName}
+      />
+    );
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
