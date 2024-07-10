6d0f338486300218ec3c30084310a3efcf3e3c8f
FridaSvanberg <svanberg92@gmail.com>
2024-07-10 16:56:27 +0200

    Clean up app.jsx and remove comments

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,34 +1,4 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
 export default function GameBoard({ onSelectSquare, board }) {
-  // let gameBoard = initialGameBoard;
-
-  // for (const turn of turns) {
-  //   const { square, player } = turn;
-  //   const { row, col } = square;
-
-  //   gameBoard[row][col] = player;
-  // }
-
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((prevGameBoard) => {
-  //       //    States that are objects or arrays should not be mutated directly.
-  //       //     It should instead be a (deep) copy. Read more about 'Reference vs Primitive Values'.
-  //       const updatedBoard = [
-  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //       ];
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedBoard;
-  //     });
-  //     onSelectSquare();
-  //   }
-
   return (
     <ol id="game-board">
       {board.map((row, rowIndex) => (
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,16 +10,6 @@ export default function Player({
   const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
-    /* If you would run this function isEditing will not go from true to false, as it might seem.
-        Because of React scheduling, both of these lines will think that isEditing was false,
-        so both lines will do the same. React will only run the handleEditClick function
-            setIsEditing(!isEditing); => true
-            setIsEditing(!isEditing); => true
-        But if you use a function in the setIsEditing twice, it will be first true and then false.
-        Because when you run it as a function, React will run them both inside the handleEditClick function.
-            setIsEditing((editing) => !editing); => true
-            setIsEditing((editing) => !editing); => false
-    */
     setIsEditing((editing) => !editing);
     if (isEditing) {
       onChangeName(symbol, playerName);
